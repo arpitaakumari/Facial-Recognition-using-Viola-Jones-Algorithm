9876b4b01306a0e57fb67101efe18798bee82ea9
Arpita Kumari <60249904+arpitaakumari@users.noreply.github.com>
2020-06-09 18:15:46 +0530

    Add files via upload

---
+++ face_recognition.py
@@ -0,0 +1,36 @@
+#face recognition
+
+#importing the libraries
+import cv2
+
+#loading the cascades
+face_cascade = cv2.CascadeClassifier('C:\\Users\\ARPITA KUMARI\\.spyder-py3\\facerecognition\\haarcascade_frontalface_default.xml')
+#eye_cascade = cv2.CascadeClassifier('C:\\Users\\ARPITA KUMARI\\.spyder-py3\\facerecognition\\haarcascade_eye.xml')
+#smile_cascade = cv2.CascadeClassifier('C:\\Users\\ARPITA KUMARI\\.spyder-py3\\facerecognition\\haarcascade_smile.xml')
+#open cv only work on gray images
+#defining a function that will do the functions
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  
+    for(x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        #eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)  #faces are a tuple fo 4 elements x,y,w,h,h
+        #for(ex, ey, ew, eh) in eyes:
+        #    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
+        #smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)  #faces are a tuple fo 4 elements x,y,w,h,h
+        #for(sx, sy, sw, sh) in smiles:
+        #   cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0,0,255), 2)
+    return frame
+
+#doing some face recognition with the webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
